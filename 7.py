3ae957443e129d65512d176465e307788b2343f0
kt-tm <katya.melnikowa@mail.ru>
2020-09-22 00:31:36 +0300

    Выполненные задания для 5-ой лекции

--- 7.py
+++ 7.py
@@ -6,5 +6,5 @@ with open("text_7.txt", "r", encoding="utf-8") as f:
         firm.update({i.split(" ")[0]: float(i.split(" ")[2]) - float(i.split(" ")[3])})
         av = av + float(i.split(" ")[2]) - float(i.split(" ")[3])  if float(i.split(" ")[2]) - float(i.split(" ")[3]) > 0.0 else av
 list = [firm, {'average_profit': av}]
-with open("7.json", "w") as write_f:
+with open("7.json", "w", encoding="utf-8") as write_f:
     json.dump(list, write_f, indent=4)
